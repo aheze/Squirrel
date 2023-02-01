0b22443e4759dc67e0cb4a55bfd97a8d8fb0828e
Zheng <aheze@getfind.app>
2023-01-31 20:15:37 -0800

    Initial Commit

---
+++ Squirrel/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Squirrel
+//
+//  Created by A. Zheng (github.com/aheze) on 1/31/23.
+//  Copyright © 2023 A. Zheng. All rights reserved.
+//
+    
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Squirrel/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Squirrel
+//
+//  Created by A. Zheng (github.com/aheze) on 1/31/23.
+//  Copyright © 2023 A. Zheng. All rights reserved.
+//
+    
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
